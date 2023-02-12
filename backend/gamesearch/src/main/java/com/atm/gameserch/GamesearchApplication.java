355006b8a9bc7be84eccc9910c2a1a89ba9df71d
atircio <atircio12@gmail.com>
2023-02-12 19:30:09 +0100

    Project Created

---
+++ backend/gamesearch/src/main/java/com/atm/gameserch/GamesearchApplication.java
@@ -0,0 +1,13 @@
+package com.atm.gameserch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesearchApplication.class, args);
+	}
+
+}
---
+++ backend/gamesearch/src/test/java/com/atm/gameserch/GamesearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atm.gameserch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
