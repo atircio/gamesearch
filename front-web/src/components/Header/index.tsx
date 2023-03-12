c6da296d37a0f9d41215e345ad55c0618c60c4c7
atircio <atircio12@gmail.com>
2023-03-12 18:09:53 +0100

    Creating routr navigation

--- front-web/src/components/Header/index.tsx
+++ front-web/src/components/Header/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+
 import './style.css'
 
 const Header = () => (
     <header className="main-header">
-        <Logo/>
-        <div className="logo-text">
-        <span className="logo-text-1">Game</span>
-        <span className="logo-text-2"> Search</span>
-
-        </div>
+        <Logo />
+        <Link to={'/'}>
+            <div className="logo-text">
+                <span className="logo-text-1">Game</span>
+                <span className="logo-text-2"> Search</span>
+            </div>
+        </Link>
     </header>
 );
 
